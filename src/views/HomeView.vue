ca74c8af9f4fd6aeb893493585ef2606c607a804
andriferry <ferryandri35@gmail.com>
2024-05-25 10:14:44 +0700

    Update Instlation DaisyUi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p class="text-red-500">
+      Hello
+    </p>
+    <button
+  class="inline-block cursor-pointer rounded-md bg-red-500 text-red-500 px-4 py-3 text-center text-sm font-semibold uppercase  transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
   </main>
 </template>
