5b1d22b2b54f84c3d998bb715241d751512016e3
andriferry <ferryandri35@gmail.com>
2024-05-27 14:05:22 +0700

    Update List Menu icons

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,71 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+
+
+const menus = computed( () => {
+  return [
+    {
+      label: 'websites',
+      icon: 'mdi:globe',
+      class: route.name === 'websites' ? 'bg-secondary': ''
+    },
+    {
+      label: 'vps',
+      icon: route.name === 'vps' ? 'material-symbols:cloud-outline' : 'material-symbols:cloud',
+      class: route.name === 'vps' ? 'bg-secondary': ''
+    },
+  ]
+})
+
+
+
+
+</script>
 
 <template>
   <div class="bg-primary w-full h-screen flex">
     <div class="w-[5%] h-screen flex flex-col px-2 py-10">
-      <div class="rounded-xl flex items-center justify-center bg-white p-1.5">
+      <RouterLink to="/" class="rounded-xl flex items-center justify-center bg-white p-1.5">
         <Icon icon="streamline-emojis:cloud-2" class="text-5xl" />
+      </RouterLink>
+
+      <div class="mt-10 flex justify-center">
+        <ul class="h-screen flex flex-col gap-10">
+          <li v-for="data , index in menus" :key="index" class="flex justify-center">
+            <RouterLink :to="{name: data.label}" :class="data.class" class="p-1.5 rounded-lg">
+              <Icon :icon="data.icon" class="text-white text-4xl" />
+            </RouterLink>
+          </li>
+         
+        </ul>
+      
       </div>
     </div>
 
     <div class="w-[95%] bg-white h-screen rounded-l-[45px] p-10">
-      <RouterView />
+      <RouterView v-slot="{ Component }">
+        <Transition name="slide-fade" mode="out-in" class="bg-white">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
     </div>
   </div>
 </template>
+
+
+<style scoped>
+
+.slide-fade-up-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-up-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-up-enter-from,
+.slide-fade-up-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DefaultLayout from '@/layouts/Deefault.vue'
+import DefaultLayout from '@/layouts/Default.vue'
 import Home from '@/views/Home.vue'
+import Vps from '@/views/Vps.vue'
+import Websites from '@/views/Websites.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
       component: DefaultLayout,
       children: [
         {
+          name: 'home',
           path: '/',
           component: Home
+        },
+        {
+          name: 'vps',
+          path: '/Vps',
+          component: Vps
+        },
+        {
+          name: 'websites',
+          path: '/websites',
+          component: Websites
         }
+        
       ]
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
